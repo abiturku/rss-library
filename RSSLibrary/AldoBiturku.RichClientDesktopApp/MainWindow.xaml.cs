1b1d555bea647cf1c2dd0ade68bea2016a6a420a
Aldo Biturku <aldobiturku@gmail.com>
2021-07-05 11:17:37 +0200

    adding guid styling

--- RSSLibrary/AldoBiturku.RichClientDesktopApp/MainWindow.xaml.cs
+++ RSSLibrary/AldoBiturku.RichClientDesktopApp/MainWindow.xaml.cs
@@ -15,13 +15,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace AldoBiturku.RichClientDesktopApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private readonly PagingCollectionView _cview;
         public MainWindow()
