a2daf1d5d6e1dfee427b9508377867ac7e09c6a5
Aldo Biturku <aldobiturku@gmail.com>
2021-07-05 13:58:52 +0200

    Web app rss

---
+++ RSSLibrary/AldoBiturku.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AldoBiturku.RssLibrary;
+using AldoBiturku.Web.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AldoBiturku.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMapper _mapper;
+
+        public HomeController(ILogger<HomeController> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ViewResult Rss()
+        {
+            //get the rss from library
+            RSS rss = new RSS();
+            List<RSSModel> listRss = rss.getRSS();
+
+            //map the model
+            var rssViewModel = _mapper.Map<List<RSSModel>, List<RSSViewModel>>(listRss);
+
+            return View(rssViewModel .AsQueryable());
+        }                             
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RSSLibrary/AldoBiturku.Web/RSSProfile.cs
@@ -0,0 +1,18 @@
+﻿using AldoBiturku.RssLibrary;
+using AldoBiturku.Web.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AldoBiturku.Web
+{
+    public class RSSProfile : Profile
+    {
+        public RSSProfile()
+        {
+            CreateMap<RSSModel, RSSViewModel>();
+        }
+    }
+}
